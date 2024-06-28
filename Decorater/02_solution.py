e2457aef14f85d97abd5419f8698e918a8c3b5c0
kaushal <kaushalkumar02918@gmail.com>
2024-06-28 08:45:16 +0530

    fix

--- Decorater/02_solution.py
+++ Decorater/02_solution.py
@@ -1,12 +1,19 @@
-
 def dubg(func):
   def wrapper(*args,**kwargs):
-      return func(**args,**kwargs)
+      args_value = ', '.join(str(arg) for arg in args)
+      kwargs_value = ', '.join( f"{k}={v}" for k,v in kwargs.items())
+      print(f"Calling : {func.__name__} with args {args_value} and kwargs {kwargs_value}")
+      return func(*args,**kwargs)
   return wrapper
 
-
+@dubg
 def greet(name,greeting="Hello"):
   print(f"{greeting} , {name}")
   
+@dubg
+def hello():
+  print("Hello")
 
+
+hello()
 greet("chai",greeting="kaushal")
